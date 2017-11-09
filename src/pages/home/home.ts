024121072d77b5e14759a3d343fd8408e8b512cf
Ankit Goel <Ankit.Goel@jda.com>
2017-11-09 20:32:36 +0530

    Add capture image functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CameraPreview, CameraPreviewPictureOptions } from '@ionic-native/camera-preview';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  cameraPreview;
+  picture;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, cameraPreview: CameraPreview) {
+    this.cameraPreview = cameraPreview;
+  }
 
+  capture() {
+    // picture options
+    const pictureOpts: CameraPreviewPictureOptions = {
+      width: 1280,
+      height: 1280,
+      quality: 85
+    }
+
+    // take a picture
+    this.cameraPreview.takePicture(pictureOpts).then((imageData) => {
+      this.picture = 'data:image/jpeg;base64,' + imageData;
+      console.log('Capture image success');
+    }, (err) => {
+      console.log('Capture image failed');
+      console.log(err);
+    });
   }
 
 }
